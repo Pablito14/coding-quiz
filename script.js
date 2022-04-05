183418e1e96bb1c64c30914f3b28dd15065a4b90
teopaulo <pablo.rosales14@gmail.com>
2022-04-05 16:10:27 -0700

    working on getting event handler on start button to function

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ var q2 = "what is a true/false type called?";
 var q3 = "how do you repeat code";
 var q4 = "what is interconnected data called?";
 var q5 = "how would you control logic flow?"
-var questions = [q1, q2, q3, q4, q5]
+var questions = [q1, q2, q3, q4, q5];
+
+
+var startBtn = document.getElementById("startBtn");
+startBtn.addEventListener("click", startBtnHandler);
+
+function startBtnHandler(event){
+
+    console.log("Don't click mEeEEEEEE!");
+}
